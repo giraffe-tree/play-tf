423ad966eaf37d29b59ed252b0d24c17b15a0cde
giraffetree <15355498770@163.com>
2018-10-20 23:18:44 +0800

    udpate home 2

--- util_guide/play_matplotlib/try_matplotlib_annotation.py
+++ util_guide/play_matplotlib/try_matplotlib_annotation.py
@@ -7,7 +7,7 @@ x = np.linspace(-3, 3, 50)
 y1 = 2 * x + 1
 y2 = x ** 2
 
-plt.figure()
+plt.figure(num=1, figsize=(8, 5))
 
 # 设置取值范围
 plt.xlim((-1, 2))
@@ -16,14 +16,18 @@ plt.ylim((-2, 3))
 plt.xlabel("i am x")
 plt.ylabel("i am y")
 
-
 # 这里要加 逗号,否则 plt.legend(handles=[l1, l2], loc='best') 中会报错
-l1, = plt.plot(x, y2, label='up')
+# plot 是画线, scatter 画点
+l1, = plt.scatter(x, y2, label='up')
 l2, = plt.plot(x, y1, label='down', color="red", linewidth=1.0, linestyle="--")
 
 # handles=,labels=,
 plt.legend(handles=[l1, l2], labels=['aaa', 'bbb'], loc='best')
 
+# 标注点
+x0 = 1
+y0 = 2*x0 +1
 
+plt.scatter()
 
 plt.show()
